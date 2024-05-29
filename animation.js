c15dfb7803fa51e3746e9c8872637910be0882f1
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-05-29 23:52:04 +0530

    para

--- animation.js
+++ animation.js
@@ -13,3 +13,17 @@ tl.from(".nand",{
   opacity:0,
   stagger:0.3
 })
+tl.from(".conatiner h1",{
+  x:-700,
+  duration:0.5,
+  opacity:0,
+  // rotate:360,
+  stagger:0.2
+})
+tl.from("#fetchjoke",{
+  x:-700,
+  duration:0.5,
+  opacity:0,
+  // rotate:360,
+  stagger:0.2
+})
